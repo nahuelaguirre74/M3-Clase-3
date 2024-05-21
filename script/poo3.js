d55a7f34ca25adfb18b4c8c23c28b774eb07aeda
Nahuel Aguirre MacBook Pro <nahuelaguirre@Nahuels-MacBook-Pro.local>
2024-05-20 18:54:38 -0600

    Se agrega archivo

---
+++ script/poo3.js
@@ -0,0 +1,54 @@
+class CuentaBancaria {
+    constructor (saldoInicial){
+        this.saldo = saldoInicial;
+    }
+
+    depositar(cantidad){
+        this.saldo += cantidad;
+    }
+    retirar(cantidad){
+        if (cantidad <= this.saldo){
+            this.saldo -= cantidad;
+            return cantidad;
+        } else{
+            return 'Fondos Insuficientes';
+
+            }
+
+        }
+        verSaldo(){
+            return this.saldo;
+        }
+}
+
+const cuenta = new CuentaBancaria(1000);
+
+document.getElementById('transactionForm').addEventListener('submit',function(event){
+    event.preventDefault();
+    const amount = parseFloat(document.getElementById('amount').value);
+    const trasactionType = document.getElementById('transactionType').value;
+
+    if (trasactionType === 'deposit'){
+        cuenta.depositar(amount)
+    } else if (trasactionType === 'withdrawal'){
+        const withdrawalResult = cuenta.retirar(amount);
+        if (typeof withdrawalResult === 'string'){
+            alert(withdrawalResult);
+        }
+    }
+
+    document.getElementById('output2').innerHTML = `El saldo actual es : ${cuenta.verSaldo()}`;
+})
+
+
+
+
+// function agregarNumero(amount) {
+// let valorActual = valorActual;
+// actualizarPantalla();
+// }
+
+
+// function actualizarPantalla() {
+// document.getElementById('pantalla').textContent = verSaldo;
+// }
